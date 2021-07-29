60d95dc878cf1cf45a89ee7004d3350749cf29bf
jose Tovar <jose.tovar102397@gmail.com>
2021-07-28 21:45:29 -0600

    Add models using object syntax

---
+++ object-examples.js
@@ -0,0 +1,19 @@
+var car = {
+  name: "subaru brz",
+  milage: 34000,
+  hasDamage: false,
+  upgradres: ["intake","invidia n1 exhaust","rocket bunny widebody"],
+}
+
+var animal = {
+  name: "crocodile",
+  isColdblooded: true,
+  averageWeight: 1000,
+}
+
+var customer = {
+  store: "amazon",
+  totalOrders: 5,
+  hasAmazonPrime: true,
+  recentSearch: ["L shaped desk", "27 inch monitor", "mouse", "keyboard"],
+}
